08406972e0abe935823a83ed2308cef6395a1a74
pinkissocool <99200272+pinkissocool@users.noreply.github.com>
2023-01-12 13:19:28 -0500

    Add files via upload

---
+++ node_http.js
@@ -0,0 +1,18 @@
+const http = require('http');
+//This code gets the http and lets us use it as a variable
+const server = http.createServer((req,res)=> {
+    if(req.url === "/"){
+        res.write('Hello World');
+        res.write("This is our first server");
+        res.end();
+    }
+
+    if (req.url === '/api/courses'){
+        res.write(JSON.stringift([1,2,3]));
+        res.write('This is a list of offerings');
+        res.write(' at BTHS');
+        res.end();
+    }
+});
+server.listen(3000);
+console.log("Listening on port 3000 ...");
